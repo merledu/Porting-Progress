0c160c49c2af379021d2b2ac7e86db9d20a717da
CyberGai <91522269+CyberGai@users.noreply.github.com>
2022-01-08 19:06:43 +0500

    initial commit

---
+++ build/zephyr/include/generated/device_extern.h
@@ -0,0 +1,38 @@
+#ifndef DEVICE_EXTERN_GEN_H
+#define DEVICE_EXTERN_GEN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const struct device DEVICE_DT_NAME_GET(DT_N); /* dts_ord_0 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_chosen); /* dts_ord_1 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_flash_20000000); /* dts_ord_2 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_memory_80000000); /* dts_ord_3 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc); /* dts_ord_4 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus); /* dts_ord_5 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_0); /* dts_ord_6 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_0_S_interrupt_controller); /* dts_ord_7 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_1); /* dts_ord_8 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_1_S_interrupt_controller); /* dts_ord_9 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_2); /* dts_ord_10 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_2_S_interrupt_controller); /* dts_ord_11 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_3); /* dts_ord_12 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_3_S_interrupt_controller); /* dts_ord_13 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_4); /* dts_ord_14 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_4_S_interrupt_controller); /* dts_ord_15 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_5); /* dts_ord_16 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_5_S_interrupt_controller); /* dts_ord_17 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_6); /* dts_ord_18 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_6_S_interrupt_controller); /* dts_ord_19 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_7); /* dts_ord_20 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_cpus_S_cpu_7_S_interrupt_controller); /* dts_ord_21 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_interrupt_controller_c000000); /* dts_ord_22 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_uart_10000000); /* dts_ord_23 */
+extern const struct device DEVICE_DT_NAME_GET(DT_N_S_soc_S_clint_2000000); /* dts_ord_24 */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DEVICE_EXTERN_GEN_H */
---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,3 @@
+empty_file.o: /home/shayan/Zephyr-test/zephyr/misc/empty_file.c \
+ /home/shayan/Zephyr-test/zephyr/boards/riscv/qemu_riscv64/qemu_riscv64.dts \
+ /home/shayan/Zephyr-test/zephyr/dts/riscv/virt.dtsi
